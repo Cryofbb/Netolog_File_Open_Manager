7960e797567ad82efc25a8408f1ab2ff00f5372c
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-07-13 15:42:56 +0300

    Manager added

---
+++ src/test/java/ru/netology/manager/FileOpenManagerTest.java
@@ -0,0 +1,49 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileOpenManagerTest {
+    private FileOpenManager manager = new FileOpenManager();
+
+    @BeforeEach
+    void setup(){
+        manager.addNew(".jpg","IMG Viewer");
+        manager.addNew(".JPG", "IMG Viewer");
+        manager.addNew(".pNg", "IMG Viewer");
+        manager.addNew(".psd","");
+        manager.addNew(".txt", "Word" );
+        manager.addNew(".txt", "Excel");
+        manager.addNew(".iso", "ISO");
+    }
+    @Test
+    void showAll(){
+        System.out.println(manager.showAll());
+    }
+
+    @Test
+    void getApp(){
+        System.out.println(manager.getApp(".png"));
+    }
+
+    @Test
+    void remove(){
+        manager.removeValue(".jpg");
+        System.out.println(manager.showAll());
+
+    }
+    @Test
+    void getExtensions(){
+        System.out.println(manager.getExtensions());
+    }
+
+    @Test
+    void getApps(){
+        System.out.println(manager.getApps());
+    }
+
+}
